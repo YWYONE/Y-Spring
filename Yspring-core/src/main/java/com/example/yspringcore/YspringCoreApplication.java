902c204417556da76f4f0d669304bc755895cdc9
YWYONE <2959934472@qq.com>
2023-10-31 18:56:39 +0800

    2

---
+++ Yspring-core/src/main/java/com/example/yspringcore/YspringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.yspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YspringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YspringCoreApplication.class, args);
+	}
+
+}
---
+++ Yspring-core/src/test/java/com/example/yspringcore/YspringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YspringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
